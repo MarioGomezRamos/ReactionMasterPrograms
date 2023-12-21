a5eb7bd27daef45f8f8dcb2d97bb3f7ff37876ab
MarioGomezRamos <mgomez40@us.es>
2023-12-21 13:24:56 +0100

    Delete substitute_NRV directory

--- substitute_NRV/fres/source/cpu_time.f
+++
@@ -1,9 +0,0 @@
-      FUNCTION SECOND()
-      IMPLICIT REAL*8(A-H,O-Z)
-      REAL*4 TARRAY(2),ETIME
-
-      call cpu_time(etime)
-      second = etime
-
-      RETURN
-      END
--- substitute_NRV/fres/source/flush.f
+++
@@ -1,3 +0,0 @@
-      SUBROUTINE FLUSH(NF)
-      RETURN
-      END
